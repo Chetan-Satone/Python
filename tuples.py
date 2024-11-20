29f8dd09b4f1a0db95ccc0afd223d5eb087f358c
Chetan-Satone <chetan.k.rock@gmail.com>
2024-11-21 00:32:38 +0530

    tuples index

---
+++ tuples.py
@@ -0,0 +1,11 @@
+"""Find Index of an Element*  
+   Write a Python program to find the index of a specific element in a tuple.  
+   Input: (10, 20, 30, 40, 50) and element 30  
+   Output: 2
+"""
+
+int= (10,20,30,40,50)      # tuples can be called directly they need indirect function
+
+
+index = int.index(30)
+print(index)
