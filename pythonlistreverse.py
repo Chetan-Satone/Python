bdbc759ecca65bfea8dd1a7393e9ee4cad87a3b7
Chetan-Satone <chetan.k.rock@gmail.com>
2024-11-21 00:19:38 +0530

    reverse lists

---
+++ pythonlistreverse.py
@@ -0,0 +1,12 @@
+"""Reverse a List*  
+   Write a Python program to reverse the elements of a list without using built-in functions.  
+   Input: [1, 2, 3, 4, 5]  
+   Output: [5, 4, 3, 2, 1]"""
+
+
+
+a=[1,2,3,4,5]
+
+reversed = a[::-1]
+
+print(reversed)
