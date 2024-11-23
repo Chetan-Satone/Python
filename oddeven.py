0ad2f02e24f70f8964ccfef1b9e9e5fef1af5f95
Chetan-Satone <chetan.k.rock@gmail.com>
2024-11-23 23:27:50 +0530

    odd even

---
+++ oddeven.py
@@ -0,0 +1,13 @@
+def oddeven(num):
+
+    if(num == 0):
+        print("the number is zero ")
+    elif(num % 2 == 0):
+        print(" the number is even")
+    else:
+        print("The number id odd")
+        
+   
+
+num =int(input("Enter the number"))
+oddeven(num)        
